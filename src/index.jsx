57b679b984c64a81e0cb1c0ef9912d6e40f75efb
ComputerNerd50 <150048114+ComputerNerd50@users.noreply.github.com>
2024-01-21 17:48:59 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
